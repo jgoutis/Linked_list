9b8758588ed9169404c63f2627ac092956c4c123
jason <jgoutis@islstudent.ch>
2023-03-23 09:51:36 +0100

    Version 2

--- src/LinkedListJG.java
+++ src/LinkedListJG.java
@@ -1,2 +1,137 @@
-package PACKAGE_NAME;public class LinkedListJG {
+public class LinkedListJG {
+    private Node head;
+
+    public LinkedListJG(){
+
+    }
+
+    public void addNode(int data){
+        Node newNode = new Node(data);
+        // Check if head is null
+        if(head == null){
+            head = newNode;
+        }
+        else {
+            // Look for null pointer
+            Node currentNode = head;
+            while (currentNode.getNext() != null) {
+                currentNode = currentNode.getNext();
+            }
+            // We now know that the currentNode is point to null (end of the list)
+            currentNode.setNext(newNode);
+            newNode.setPrev(currentNode);
+        }
+    }
+
+    public void addToHead(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            this.setHead(newNode);
+        }
+        else{
+            newNode.setNext(head);
+            this.setHead(newNode);
+            newNode.getNext().setPrev(newNode);
+
+        }
+    }
+
+    public void addToPos(int pos, int data){
+
+        Node currentNode = head;
+        Node newNode = new Node(data);
+        // Check if pos is the head
+        if(pos == 0){
+            this.addToHead(data);
+        }
+        else {
+            for (int i = 0; i < pos - 1; i++) {
+                // Check if pos goes beyond of the list
+                if(currentNode.getNext() != null) {
+                    currentNode = currentNode.getNext();
+                }
+                else{
+                    this.addNode(data);
+                    break;
+                }
+            }
+
+            if(currentNode.getNext() != null) {
+                newNode.setNext(currentNode.getNext());
+                currentNode.setNext(newNode);
+                newNode.getNext().setPrev(newNode);
+                newNode.setPrev(currentNode);
+            }
+        }
+    }
+
+    public void changeData(int data, int newData){
+        Node currentNode = head;
+        while(currentNode != null){
+            if(currentNode.getData() == data){
+                currentNode.setData(newData);
+            }
+            else{
+                currentNode = currentNode.getNext();
+            }
+        }
+    }
+
+    public void delete(int data){
+        Node currentNode = head;
+        if(currentNode.getData() == data){
+            this.setHead(currentNode.getNext());
+            head.setPrev(null);
+            return;
+        }
+        currentNode = currentNode.getNext();
+        while(currentNode.getNext() != null){
+            if(currentNode.getData() == data){
+                currentNode.getPrev().setNext(currentNode.getNext());
+                currentNode.getNext().setPrev(currentNode.getPrev());
+                break;
+            }
+            else{
+                currentNode = currentNode.getNext();
+            }
+        }
+        if(currentNode.getNext() == null && currentNode.getData() == data){
+            currentNode.getPrev().setNext(null);
+        }
+    }
+
+    public String reverseOutput(){
+        String data = "";
+        Node currentNode = head;
+        while(currentNode.getNext() != null){
+            currentNode = currentNode.getNext();
+        }
+
+        while(currentNode.getPrev() != null){
+            data = data + currentNode.getData() + ", ";
+            currentNode = currentNode.getPrev();
+        }
+        data = data + currentNode.getData();
+        return data;
+    }
+
+    public String toString(){
+        String data = "";
+        Node currentNode = head;
+
+        while (currentNode.getNext() != null) {
+            data = data + currentNode.getData() + ", ";
+            currentNode = currentNode.getNext();
+        }
+        data = data + currentNode.getData();
+        return data;
+    }
+
+    private Node getHead() {
+        return head;
+    }
+
+    private void setHead(Node head) {
+        this.head = head;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,79 @@
+import org.junit.Assert;
+import org.junit.ComparisonFailure;
+
 public class Main {
+    private static int score = 0;
+    private static int tests = 0;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LinkedListJG mt = new LinkedListJG();
+
+        // Insertion and toString
+        mt.addNode(5);
+        mt.addNode(7);
+        mt.addNode(10);
+        check("Insertion and toString", "5, 7, 10", mt.toString());
+
+        // Mid-point insertion
+        mt.addToPos(1, 22);
+        check("Mid-point insertion", "5, 22, 7, 10", mt.toString());
+
+        // Tail insertion
+        mt.addToPos(3, 50);
+        check("Tail insertion", "5, 22, 7, 50, 10", mt.toString());
+
+        // Head insertion
+        mt.addToHead(1);
+        check("Head insertion", "1, 5, 22, 7, 50, 10", mt.toString());
+
+        // Check reverse output
+        String rev = mt.reverseOutput();
+        System.out.println(rev);
+        check("Reverse output", "10, 50, 7, 22, 5, 1", mt.reverseOutput());
+
+        // Check mid-point deletion
+        mt.delete(7);
+        check("Mid-point deletion", "1, 5, 22, 50, 10", mt.toString());
+
+        // Head deletion
+        mt.delete(1);
+        check("Head deletion", "5, 22, 50, 10", mt.toString());
+
+        // Tail deletion
+        mt.delete(10);
+        check("Tail deletion", "5, 22, 50", mt.toString());
+
+        // Insertion and toString test 2
+        mt.addNode(33);
+        mt.addNode(44);
+        mt.addNode(77);
+        mt.changeData(50,100);
+        check("Mid-point change", "5, 22, 100, 33, 44, 77", mt.toString());
+
+        mt.changeData(5,6);
+        check("Head change", "6, 22, 100, 33, 44, 77", mt.toString());
+
+        mt.changeData(77,88);
+        check("Tail change", "6, 22, 100, 33, 44, 88", mt.toString());
+
+        check("Insertion and toString test 2", "6, 22, 100, 33, 44, 88", mt.toString());
+
+        // Check reverse output 2
+        check("Reverse output test 2", "88, 44, 33, 100, 22, 6", mt.reverseOutput());
+
+
+        System.out.println("\nFinal Score:" + score + "/" + tests);
+    }
+
+    public static void check(String test, String expected, String actual){
+        tests += 1;
+        try {
+            Assert.assertEquals(expected, actual);
+            System.out.println("✓   " + test + " passed.");
+            score += 1;
+        }
+        catch (ComparisonFailure e){
+            System.out.println("x   " + test + " failed.");
+        }
     }
 }
